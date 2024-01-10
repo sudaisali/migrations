d625fa89991d97844f82a50c8e5a2cd28133bdf4
Sudais Bin Sohail <63845164+sudaisali@users.noreply.github.com>
2024-01-10 10:52:21 +0500

    Delete app/database directory

--- app/database/config/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('migration', 'postgres', 'admin', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
-
-
-module.exports = sequelize
