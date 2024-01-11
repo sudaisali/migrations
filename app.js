760f33b54e429f73a872bfe59e229f5c3ca48d5e
sudais ali <sudaisbinsohail@gmail.com>
2024-01-11 12:06:08 +0500

    new branch

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const Model = require('./app/database/models')
+
+
+app.use('/get-data',async (req,res,next)=>{
+    const data = await Model.billionaries.findAll(
+       {
+        attributes: [
+        'person_name','person_company_relation'
+          ],
+          include:[{
+            model:Model.companies,
+            attributes:['company_name','company_founded'],
+            include:[
+                {
+                    model:Model.sectors,
+                    attributes:['sector_name']
+                }
+            ]
+          }],
+        
+       }
+    
+    )
+    console.log(data)
+    res.json(data)
+})
+
+
+app.listen(3001,()=>{
+    console.log("server is connected")
+})
